b4e963a698ea247740e4561147a29136568f43c6
Nama-Salameh <nama.salameh@gmail.com>
2026-03-18 11:30:14 +0200

    feat: setup pg-boss job queue

--- src/queue/boss.ts
+++ src/queue/boss.ts
@@ -3,13 +3,9 @@ import { env } from "../config/env.js";
 
 export const boss = new PgBoss({
   connectionString: env.DATABASE_URL,
-  schema: "public",
 });
 
 export const startQueue = async () => {
   await boss.start();
   console.log("PgBoss queue started");
-
-  await boss.createQueue("process_event");
-  console.log("Queue 'process_event' created");
 };
